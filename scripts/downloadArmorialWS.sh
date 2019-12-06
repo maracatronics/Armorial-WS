a32047808b8f83238c4e6606611a8775ccada5bc
Pedro Jorge <pjls2@cin.ufpe.br>
2019-12-05 18:24:57 -0800

    alteracao no script de download e instalacao do ws

--- scripts/downloadArmorialWS.sh
+++ scripts/downloadArmorialWS.sh
@@ -1,24 +1,23 @@
 
 scriptWD=`pwd`
 
-echo "Installing SFML and Qt5"
-sudo apt-get update
-sudo apt-get upgrade
+echo "updating apt repo"
+sudo apt update
+sudo apt upgrade
 
-sudo apt-get install build-essential libsfml-dev
+echo "Installing SFML and Qt5"
+
+sudo apt install build-essential libsfml-dev g++ libqt4-dev libeigen3-dev protobuf-compiler libprotobuf-dev \ 
+libdc1394-22 libdc1394-22-dev cmake libv4l-0 libopencv-dev freeglut3-dev qt5-default omniorb omniidl omniorb-nameserver libomniorb4-dev
 
 wget http://download.qt.io/official_releases/qt/5.13/5.13.2/qt-opensource-linux-x64-5.13.2.run
 
-chmod +x qt-opensource-linux-x64-5.13.2
-./qt-opensource-linux-x64-5.13.2
+chmod +x ./qt-opensource-linux-x64-5.13.2.run
+./qt-opensource-linux-x64-5.13.2.run
 
 sudo rm qt-opensource-linux-x64-5.13.2.run
 
 echo "Installing dependencies to GEARSystem and WRBackbone"
-sudo apt-get update
-sudo apt-get upgrade
-
-sudo apt-get install omniorb omniidl omniorb-nameserver libomniorb4-dev
 
 cd $scriptWD/..
 
@@ -38,6 +37,7 @@ cd $scriptWD/..
 echo "Installing WRBackbone"
 git clone https://github.com/maracatronics/WRBackbone.git
 
+cd WRBackbone
 cd trunk/
 mkdir build
 cd build/
@@ -49,8 +49,8 @@ make -j4
 cd $scriptWD/..
 
 echo "Installing Armorial-Carrero"
-
 git clone https://github.com/maracatronics/Armorial-Carrero.git
+cd Armorial-Carrero
 sudo sh protobuf.sh
 cd build
 qmake ..
@@ -59,13 +59,12 @@ make
 cd $scriptWD/..
 echo "placeHolder to Armorial-Suassuna"
 
-read -p "Do you want to install the Armorial-chico firmware repository?" -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-    echo "Downloading Armorial-Chico"
-    git clone https://github.com/maracatronics/Armorial-Carrero.git
-then
-    echo "Bye-bye :)"
-    exit 1
-fi
+read -p "Do you wish to install the Armorial-chico firmware repository?" yn
+case $yn in
+    [Yy]* ) echo "Downloading Armorial-Chico"; git clone https://github.com/maracatronics/Armorial-Chico.git;;
+    [Nn]* ) echo "Bye-bye :)";exit;;
+    * ) echo "Please answer yes or no.";;
+esac
+
+
 
