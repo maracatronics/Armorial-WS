66daaeadff5b8e3bda0c95404889b606c426437a
Pedro Jorge <pjls2@cin.ufpe.br>
2020-02-12 20:07:34 -0300

    ajuste na instalacao do qt para deixar opcional

--- scripts/downloadArmorialWS.sh
+++ scripts/downloadArmorialWS.sh
@@ -5,16 +5,22 @@ echo "updating apt repo"
 sudo apt update
 sudo apt upgrade
 
-echo "Installing SFML and Qt5"
+echo "Installing Dependencies"
+
+
 
 sudo apt install build-essential libsfml-dev g++ libqt4-dev libeigen3-dev protobuf-compiler libprotobuf-dev libdc1394-22 libdc1394-22-dev cmake libv4l-0 libopencv-dev freeglut3-dev qt5-default omniorb omniidl omniorb-nameserver libomniorb4-dev
 
-wget http://download.qt.io/official_releases/qt/5.13/5.13.2/qt-opensource-linux-x64-5.13.2.run
-
-chmod +x ./qt-opensource-linux-x64-5.13.2.run
-./qt-opensource-linux-x64-5.13.2.run
-
-sudo rm qt-opensource-linux-x64-5.13.2.run
+read -p "Do you wish to install QT Creator?" yn
+case $yn in
+    [Yy]* ) echo "Downloading QT"; 
+        wget http://download.qt.io/official_releases/qt/5.13/5.13.2/qt-opensource-linux-x64-5.13.2.run;
+        chmod +x ./qt-opensource-linux-x64-5.13.2.run;
+        ./qt-opensource-linux-x64-5.13.2.run;
+        sudo rm qt-opensource-linux-x64-5.13.2.run;;
+    [Nn]* ) echo "Bye-bye :)";;
+    * ) echo "Please answer yes or no.";;
+esac
 
 echo "Installing dependencies to GEARSystem and WRBackbone"
 
