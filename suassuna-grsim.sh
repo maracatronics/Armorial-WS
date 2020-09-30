1e8a11d5f62a2054a09400beaf1d60d73fbb7670
Pedro Jorge <pjls2@cin.ufpe.br>
2020-09-30 16:20:32 -0300

    scripts to docker execution

---
+++ suassuna-grsim.sh
@@ -0,0 +1,22 @@
+scriptWD=`pwd`
+
+sudo service omniorb4-nameserver restart
+cd WRBackbone
+cd trunk
+cd build
+cd bin
+./WRBackbone &> /dev/null &
+
+sleep 5
+cd $scriptWD
+cd Armorial-Actuator/bin
+./Armorial-SimActuator &> /dev/null &
+
+cd $scriptWD
+cd ./Armorial-Carrero/bin
+./Armorial-Carrero &> /dev/null &
+
+
+cd $scriptWD
+cd ./Armorial-Suassuna/bin
+./Armorial-Suassuna &> /dev/null &
