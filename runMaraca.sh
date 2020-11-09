9cdc8330ed67f1e6ac83630fdfc370e29449a31d
zsmn <zsmn@cin.ufpe.br>
2020-11-09 19:56:56 -0300

    More scripts

---
+++ runMaraca.sh
@@ -0,0 +1,2 @@
+# sudo sh suassuna.sh ip-actuator port-actuator ip-vision port-vision teamColor teamSide isAgainstThog
+sudo sh suassuna.sh 127.0.0.1 20011 224.5.23.2 10002 yellow right true
--- suassuna.sh
+++ suassuna.sh
@@ -1,12 +1,12 @@
 scriptWD=`pwd`
 
 # how to run
-# sudo sh suassuna.sh ip-actuator port-actuator ip-vision port-vision teamColor teamSide
+# sudo sh suassuna.sh ip-actuator port-actuator ip-vision port-vision teamColor teamSide isAgainstThog
 
 sudo rm -rf /var/lib/omniorb/*
 sudo service omniorb4-nameserver restart
 
-sleep 5
+sleep 1
 
 cd /home/Armorial-WS/
 cd WRBackbone
@@ -15,19 +15,19 @@ cd build
 cd bin
 ./WRBackbone &> /dev/null &
 
-sleep 5
+sleep 1
 cd /home/Armorial-WS/
 cd Armorial-Actuator/bin
 ./Armorial-SimActuator $1 $2 &> /dev/null &
 
-sleep 5
+sleep 1
 
 cd /home/Armorial-WS/
 cd ./Armorial-Carrero/bin
 ./Armorial-Carrero $3 $4 &> /dev/null &
 
-sleep 5
+sleep 1
 
 cd /home/Armorial-WS/
 cd ./Armorial-Suassuna/bin
-./Armorial-Suassuna $5 $6 false &> /dev/null &
+./Armorial-Suassuna $5 $6 false $7 &> /dev/null &
